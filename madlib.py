6be9ef2b49cd5dbb35ba98df661135c2dba2f638
Maxwell Tieche <max@Maxwells-MacBook-Pro.local>
2018-09-13 14:45:13 -0400

    Solution includes 3 defintions.

--- madlib.py
+++ madlib.py
@@ -1,5 +1,16 @@
-Noun=str(input("Enter a noun:  "))
-Verb=str(input("Enter a verb:  "))
-Adjective=str(input("Enter an adjective:  "))
-Place=str(input("Enter a place:  "))
-print ("Take your " + Adjective + " " + Noun + " and " + Adjective + " it at the " + Place + "!")
+def promptForWords():
+    global Noun, Verb, Adjective, Place 
+    Noun=str(input("Enter a noun:  "))
+    Verb=str(input("Enter a verb:  "))
+    Adjective=str(input("Enter an adjective:  "))
+    Place=str(input("Enter a place:  "))
+    
+def makeAndPrintSentence():
+    Sentence = ("Take your " + str(Adjective) + " " + str(Noun) + " and " + str(Verb) + " it at the " + str(Place) + "!")
+    print (Sentence)
+    
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+    
+main()
