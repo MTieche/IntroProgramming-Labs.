9124f3fd80f6fcdaf69258e01cc9247649aaf7e3
Maxwell Tieche <max@Maxwells-MacBook-Pro.local>
2018-09-20 15:10:35 -0400

    Wrote an interactive Fibonacci calculator.

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+import math
+
+n=int(input("Enter n'th number of the Fibonacci Sequence : "))
+PAns=0
+Ans=1
+for i in range (n):
+    print (PAns)
+    PAns,Ans=Ans,PAns+Ans
+    
+    
+
+   
+
+    
+    
+    
+    
