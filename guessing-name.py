adfc4357fd8b29acc302b6fb5781e11c99338ff6
Maxwell Tieche <max@Maxwells-MacBook-Pro.local>
2018-10-18 14:43:43 -0400

    Usability improvements to the guessing game, resolves #5 and #6 Issues.

--- guessing-name.py
+++ guessing-name.py
@@ -13,7 +13,7 @@ def main():
     while res==True:
         print("I'm thinking of an animal")
         guess=str(input("Guess the animal:  "))
-        if guess.lower() == "quit":
+        if guess[:1] == "q" or guess[:1] == "Q":
             print ("Goodbye")
             sys.exit()
         elif guess.lower() != "elephant":
@@ -22,6 +22,11 @@ def main():
             res=False
     print ("Congrats, you have finally got it \n"
        "Took you long enough")
+    likeness=str(input("Do you like elephants: (Yes or No)  "))
+    if likeness[:1] == "y" or likeness[:1] == "Y":
+        print ("Good.")
+    else:
+        print ("I am disappointed")
 main()
 
     
